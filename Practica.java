a7a49ecd4301d051498a4c5b114f234f2081f490
Franco <franciscojavierelpro@gmail.com>
2022-04-29 13:03:35 -0500

    hola como ta

--- Practica.java
+++ Practica.java
@@ -12,5 +12,6 @@ public class Practica {
 
         System.out.println("Tu no mete cabra saramambiche");
         System.out.println("Ye, estoy subiendo como espuma");
+        System.out.println("Prendiendole en la cara al que no fuma");
     }
 }
