503f676f266d653607de8affe6e521c074089f1d
Franco <franciscojavierelpro@gmail.com>
2022-04-28 22:05:20 -0500

    mi primer commit

---
+++ Practica.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import javax.swing.*;
+
+public class Practica {
+    public static void main(String[] args) {
+        Scanner consola = new Scanner(System.in);
+        String name;
+
+
+        name = JOptionPane.showInputDialog("Tu nombre: ");
+        JOptionPane.showMessageDialog(null,"Hola cómo estás" + name);
+    }
+}
